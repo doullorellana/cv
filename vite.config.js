ad7b1fed37e3167f1dd1676d0506feeca4c43857
Doull Orellana <doullorellana@gmail.com>
2023-10-09 20:03:19 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://doullorellana.github.io/"
+  base: "https://doullorellana.github.io/cv"
 })
